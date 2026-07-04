e317759a62962d1614b056debf97cd88c0d3a13d
Juan Rodriguez <juanprodrigues.33@gmail.com>
2026-07-04 17:21:01 -0300

    feat: implement Observability

--- billing-service/src/main/java/com/ecommerce/billing/service/BillingServiceApplication.java
+++ billing-service/src/main/java/com/ecommerce/billing/service/BillingServiceApplication.java
@@ -1,7 +1,10 @@
 package com.ecommerce.billing.service;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class BillingServiceApplication {
@@ -10,4 +13,11 @@ public class BillingServiceApplication {
 		SpringApplication.run(BillingServiceApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner runner(Environment env) {
+	    return args -> {
+	        System.out.println("BOOTSTRAP = " +
+	                env.getProperty("spring.kafka.bootstrap-servers"));
+	    };
+	}
 }
